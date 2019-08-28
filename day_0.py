7a2f04fa5241fad720ae7cca04ee48292f7e9929
inarchus <inarchus@gmail.com>
2019-08-28 08:52:03 -0400

    First Commit

---
+++ day_0.py
@@ -0,0 +1,43 @@
+
+print('hello there python')
+
+print(5)
+print(7.2)
+print("What is the meaning of life?")
+print(None, True, False)
+print(5, 8.5, "The meaning of life is 42.", '\u2770', 'wait what was that?')
+
+x = 3
+y = 5
+print(x, y)
+
+s = "i am a string, print me please"
+b = True    # i am a boolean value
+r = 3.2
+
+print(s, b, r)
+
+five_plus_two = 5 + 2
+two_strings = 'hello' + 'world'
+square_root_of_three = 3 ** (1/2)
+square_root_of_three_maybe = 3 ** 1/2
+print(square_root_of_three, square_root_of_three_maybe)
+
+x = 15
+y = 26 + x
+z = 3 * x + 16 * y + 3.14159265358979**2
+print(x, y, z)
+
+# the_error = 'hello' + True
+# another_error = 2 + 's'
+# what = 2 + '3'
+
+# import time
+# time.sleep(1)
+
+# print(the_man_with_no_name)
+
+three_var = int('3')
+pi = float('3.141592653589793238')
+a_string_var = str(5) + str(3.2)
+print(three_var, pi, a_string_var)
